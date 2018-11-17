d7e3861ba971409b16a39ca6d2218092b9161d56
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2018-11-17 12:06:11 -0800

    Add an async-await test
    This requires targeting es2015.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  private tesst = async () => {
+    await fetch('');
+  }
+
   render() {
     return (
       <div className="App">
