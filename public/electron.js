73a4a1dcf7c8e0bc66c722c75100435fe0804f59
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2019-12-28 15:40:46 -0800

    Correct Visual Studio debugging

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,6 @@
 // This file is here to quiet electron-builder.  Without it, it complains about
 // it missing even though it's not referenced anywhere in this application.
-// Adding "main": "build/electron.js", the desired entry point that is usually
-// in the public folder, to package.json does not quiet it.  Targeting the
+// Having "main": "build/electron.js", the desired entry point that is usually
+// in the public folder, in package.json does not quiet it.  Targeting the
 // public folder as the output of the main process build process comes with its
 // own problems.
