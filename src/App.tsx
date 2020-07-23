40db7cc4faeef845cc27aa4147941a3675b16d19
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2020-07-22 20:45:57 -0700

    Improve content and format

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.sass';
 import Hello from './components/Hello';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            Edit <code>src/App.tsx</code> or any other file in the <code>src</code> directory and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
         </header>
         <Hello name="George" enthusiasmLevel={5} />
       </div>
