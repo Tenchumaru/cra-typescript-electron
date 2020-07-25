37c5cdbabba7c0e5784229e07c12e9b7b1491cb5
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2020-07-25 15:57:22 -0700

    Update preload.ts

--- main/preload.ts
+++ main/preload.ts
@@ -1,21 +1,25 @@
-const { contextBridge, ipcRenderer } = require('electron');
+import { contextBridge, ipcRenderer, MessageBoxOptions, OpenDialogOptions, SaveDialogOptions } from 'electron';
+
+type MessageBoxType = 'none' | 'info' | 'error' | 'question' | 'warning';
+type WithKind<T, K> = T & { kind: K; };
+type Request = WithKind<MessageBoxOptions, 'showMessageBox'> | WithKind<OpenDialogOptions, 'showOpenDialog'> | WithKind<SaveDialogOptions, 'showSaveDialog'>;
 
 contextBridge.exposeInMainWorld('main', {
-  showMessageBox: (message, buttons, title, type) => {
+  showMessageBox: (message: string, buttons?: string[], title?: string, type?: MessageBoxType): Promise<number> => {
     return request({ kind: 'showMessageBox', buttons, message, title, type });
   },
-  showOpenDialog: (defaultPath, title) => {
+  showOpenDialog: (defaultPath?: string, title?: string): Promise<string | undefined> => {
     return request({ kind: 'showOpenDialog', defaultPath, title });
   },
-  showSaveDialog: (defaultPath, title) => {
+  showSaveDialog: (defaultPath?: string, title?: string): Promise<string | undefined> => {
     return request({ kind: 'showSaveDialog', defaultPath, title });
   },
 });
 
-function request(request) {
+function request<T>(request: Request): Promise<T> {
   ipcRenderer.send('request', request);
-  return new Promise((resolve, _reject) => {
-    // Do not send the event since it includes the sender.
+  return new Promise<T>((resolve, _reject) => {
+    // Do not include the event since it includes the sender.
     ipcRenderer.once('response', (_event, response) => resolve(response));
   });
 }
